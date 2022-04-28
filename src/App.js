f0d4b5780bed97bc8a3c3741e07fed9a0da1d48c
Prateek Varma <prtk.varma@gmail.com>
2022-04-28 10:49:41 -0600

    created the search component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import { BiArchive } from "react-icons/bi"
+import Search from "./components/Search"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
-      <h1> <BiArchive /> Your appointment</h1>
+      <h1> 
+        <BiArchive className="inline-block" /> Your appointment
+        <Search />
+      </h1>
     </div>
   );
 }
